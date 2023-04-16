18e51f4502288c2bb9a6cefb8b30620995170ba4
Justin A Oeters <justin.oeters@gmail.com>
2023-04-16 13:56:44 -0400

    pre router

--- oeterstravelblog/src/home.js
+++ oeterstravelblog/src/home.js
@@ -1,26 +1,19 @@
 import {useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setblogs] = useState([
-        {title: 'my new website', body: 'Some text', author: 'Gus', id: 1},
-        {title: 'my second website', body: 'Some text', author: 'Justin', id: 2},
-        {title: 'my third website', body: 'Some text', author: 'Gus', id: 3}
-    ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setblogs(newBlogs);
-    }
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
-    useEffect(() =>{
-        console.log('use effect ran')
-    });
+   
 
      return ( 
         <div className="home">
             <h2>Home Page</h2>
-         <BlogList blogs={blogs} handleDelete={handleDelete}/>
+            { error && <div>{error}</div>}
+            { isPending && <div>LOADING...</div>}
+         {blogs && <BlogList blogs={blogs} /> }
        
         </div>
       );
